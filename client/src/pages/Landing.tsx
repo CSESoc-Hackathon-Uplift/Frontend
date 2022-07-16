453a6b903ba8cc70cb1a4d6a6a8b19a9fe5bf251
gracexkan <grace.kan38@gmail.com>
2022-07-17 00:47:29 +1000

    updated

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
-import { Container, Card, Grid } from '@mui/material'
+import { Container, Card, Grid, Box, Typography } from '@mui/material'
 
 const Landing = () => {
   return (
-    <Container>
-      <Grid container spacing={2} paddingY={2} sx={{ maxWidth: 'lg' }}>
-        <Grid item xs={12}>
-          landing page
-        </Grid>
-      </Grid>
+    <Container sx={{ display: 'flex', flexDirection: 'row', height: '300px' }}>
+      <Container sx={{ width: '30%', justifyContent: 'centre' }}>
+        <Typography variant="h1" sx={{ fontWeight: 'bold' }}>Uplift</Typography>
+      </Container>
+      <Container></Container>
     </Container>
   )
 }
--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,13 +1,25 @@
 import React from 'react'
 
-import { Grid, Container, Typography } from '@mui/material'
+import { Box, Grid, CardMedia, Container, LinearProgress, Typography } from '@mui/material'
+import { LazyLoadImage } from 'react-lazy-load-image-component'
+import profile from '../assets/profile_pic.jpg'
+import { styled } from '@mui/material/styles'
 
 const Profile = () => {
   return (
     <Container>
       <Grid container spacing={2} paddingY={2} sx={{ maxWidth: 'lg' }}>
         <Grid item xs={12}>
-          profile page
+          <Box sx={{ display: 'flex', flexDirection: 'row', gap: '80px' }}>
+            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+              <CardMedia>
+                <LazyLoadImage src={profile} alt={'profile pic'} height="200px" width="200px" effect={'opacity'} style={{ borderRadius: '15px', aspectRatio: '394/267', objectFit: 'cover' }} />
+              </CardMedia>
+              <Typography variant="h5">Username</Typography>
+              <Box></Box>
+            </Box>
+            <LinearProgress />
+          </Box>
         </Grid>
       </Grid>
     </Container>
